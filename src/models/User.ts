57d0367e670c66cb7a2d6be9f933ca3ac2dd300f
mutsinziisaac <mutsinziisaac123@gmail.com>
2024-05-13 14:20:31 +0300

    moving model directory form root to src

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from 'sequelize';
-import sequelize from '../sequelizeConfig';
+import sequelize from '../../sequelizeConfig';
 
 interface UserAttributes {
   id: number;
