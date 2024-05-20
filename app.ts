3f9315bac1a21e8444bfb37b346632b39b705a3d
kennyg37 <k.ganza@alustudent.com>
2024-05-20 10:06:30 +0200

    swagger jsodc setup merge conflicts fixed

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import swaggerUi from 'swagger-ui-express';
+import specs from './swagger.config';
+
+const app = express();
+
+app.use(express.json());
+
+// Route handler with underscore to indicate unused parameter
+app.get('/', (_req: Request, res: Response) => {
+    return res.json({ message: "welcome to ATLP Backend APIs" });
+});
+// Swagger UI route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+export default app;
