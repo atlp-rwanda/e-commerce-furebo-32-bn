4a1c2552b0493d6394e0e627bb892d8173f6f914
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-05-30 12:21:44 +0200

    User logout functionality

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,6 @@
 import express from 'express';
-<<<<<<< HEAD
-import { userSignup,userLogout } from '../controllers/user.controller';
-=======
-import { updateRole, userSignup } from '../controllers/user.controller';
+import { updateRole, userSignup,userLogout } from '../controllers/user.controller';
 import { protectRoute, restrictTo } from '../middlewares/auth.middleware';
->>>>>>> main
 import userLogin from '../controllers/user.controller';
 import { validateUser,validateUserLogin } from '../validations/user.validate';
 import { verifyToken } from '../middleware/authMiddleware';
