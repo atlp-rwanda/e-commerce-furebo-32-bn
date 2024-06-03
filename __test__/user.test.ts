b5c8e9de3490533329c1a26440cd13063f7969f9
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-06-03 14:24:58 +0200

    Test

--- __test__/user.test.ts
+++ __test__/user.test.ts
@@ -344,29 +344,7 @@ describe("User", () => {
       expect(res.statusCode).toBe(401);
       expect(res.body).toHaveProperty("message");
     });
-  
-  
-  test("Error handling during logout", async () => {
-    // Simulate an error during logout
-    jest.spyOn(console, 'error').mockImplementation(() => {});
-    const loginUser = {
-      email: "mugisha@gmail.com",
-      password: process.env.TEST_USER_PASS,
-    };
-    const loginRes = await request(app).post('/api/users/login').send(loginUser);
-    const token = loginRes.body.token;
-  
-    // Logout using the obtained token
-    const res = await request(app)
-      .post('/api/users/logout')
-      .set('Authorization', `Bearer ${token}`);
-  
-    if (res.statusCode !== 200) {
-      expect(console.error).toHaveBeenCalled();
-    }
-    expect(res.statusCode).toBe(200);
   });
-});
   
 describe("Testing endpoint", () => {
   test("Not found for site 404", async () => {
